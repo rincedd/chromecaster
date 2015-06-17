8dad40c1f3369fab5759fe3be8f5cf73879d0f26
Gerd Zschaler <gerd@gzschaler.de>
2015-06-17 23:27:56 +0200

    Add first real test for media loading

--- test/common.js
+++ test/common.js
@@ -1,5 +1,9 @@
 import chai from 'chai';
 import asPromised from 'chai-as-promised';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 
 chai.use(asPromised);
+chai.use(sinonChai);
 global.expect = chai.expect;
+global.sinon = sinon;
--- test/media.js
+++ test/media.js
@@ -1,13 +1,44 @@
-import Chromecaster from '../lib/chromecaster';
+import MediaManager from '../lib/mediamanager';
 
 describe('Media loading', () => {
-    let caster;
+    let manager, session, media = {};
 
     beforeEach(() => {
-        caster = new Chromecaster();
+        media = {
+            addUpdateListener: sinon.spy()
+        };
+        session = {
+            media: [],
+            addMediaListener: sinon.spy(),
+            loadMedia: sinon.stub().callsArgWith(1, media)
+        };
+        manager = new MediaManager(session);
     });
 
     it('should return a promise', () => {
-        expect(caster.loadMedia()).to.be.an.instanceof(Promise);
+        expect(manager.loadMedia()).to.be.an.instanceof(Promise);
+    });
+
+    describe('mediaLoaded event', () => {
+        let listener;
+
+        beforeEach(() => {
+            listener = sinon.spy();
+            manager.on('mediaLoaded', listener);
+        });
+
+        it('should emit mediaLoaded on success', () => {
+            let promise = manager.loadMedia();
+            expect(listener).to.have.been.called;
+            return expect(promise).to.become(media);
+        });
+
+        it('should not emit mediaLoaded event on failure', () => {
+            session.loadMedia = sinon.stub().callsArgWith(2, new Error('Fail'));
+
+            let promise = manager.loadMedia();
+            expect(listener).not.to.have.been.called;
+            return expect(promise).to.be.rejected;
+        });
     });
 });
